fcf0a6a618f6c6c60190ff65e088160eb0690fdf
Abdulla Khalitov <abdullahalitoff@gmail.com>
2026-09-06 15:59:17 +0300

    ff

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,16 +1,11 @@
 
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
-
 import { apartments } from "@/src/api/apartmemts";
-import { useFavorites } from "@/src/hooks/useFavorites";
 
-import styles from "@/src/components/apartments-list/apartments-list.module.css";
 import FavoritesSection from "../../components/favorites-section/favorites-section";
-import Header from "../../components/header/header";
-import Footer from "../../components/Footer/Footer";
+import Header from "@/src/components/header/header";
+import Footer from "@/src/components/Footer/Footer";
 
 export default function FavoritesPage() {
   return (
